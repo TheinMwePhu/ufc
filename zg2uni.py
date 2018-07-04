4ba420a925df21c7030f5d1c8af7d5d7d7cb95d7
TheinMwePhu <theinmwephu97@gmail.com>
2018-07-04 00:01:46 -0700

    added ya_yit

--- zg2uni.py
+++ zg2uni.py
@@ -3,8 +3,9 @@ import re
 
 
 def convert(input):
-	output = input
+    output = input
 
-	output = output.replace(u'\u106A', u'\1009')
+    output = output.replace(u'\u106A', u'\u1009')
+    output = re.sub(u'[\u103b\u107e\u107f\u1080\u1081\u1082\u1083\u1084]', u'\u103c', output)
 
-	return output
+    return output
